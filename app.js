4130ba9166ea13025afeddd73d009082a8ab0410
Murat <sapedit@hotmail.com>
2020-04-13 14:51:56 +0300

    up: to express server

--- app.js
+++ app.js
@@ -1,60 +1,9 @@
+const express = require("express");
+const app = express();
+app.use(express.static("public"));
 
-var express=require('express');
-var mysql = require('mysql');
-var app=express();
-var fs=require('fs');
-var http=require('http');
-var reload=require('reload');
+const port = process.env.PORT || 3000;
 
-
-
-let connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'aknels',
-  password: 'xkg5zeze',
-  database: 'sampledb',
-});
-
-connection.connect(function (err) {
-  if (!!err) {
-  console.log('olmadı'); 
-} else {
-    console.log('MySQL bağlantısı başarıyla gerçekleştirildi.'); 
-  }
-  
-});
-
-
-app.get('/',function(req,res) {
-  connection.query("SELECT*FROM mySampleTable",function(err,rows,fields){
-       if (!!err) {
-         console.log('hata');
-       }   else {
-           console.log('Başarılı...');
-      }
-
-  })
-});
-
-
-var sunucu=http.createServer(function(req,res){
-   fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-   if (err) {
-    res.writeHead(500);
-    return res.end('Error loading index.html');
-   }
- 
-   res.writeHead(200);
-   res.end(data);
-  }); 
-
-});
-
-
-
-sunucu.listen(3000);
-reload(app)
-
-
-
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
---
+++ app_OLD.js
@@ -0,0 +1,46 @@
+var express = require("express");
+// var mysql = require('mysql');
+var app = express();
+var fs = require("fs");
+var http = require("http");
+var reload = require("reload");
+
+// let connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'aknels',
+//   password: 'xkg5zeze',
+//   database: 'sampledb',
+// });
+
+// connection.connect(function (err) {
+//   if (!!err) {
+//   console.log('olmadı');
+// } else {
+//     console.log('MySQL bağlantısı başarıyla gerçekleştirildi.');
+//   }
+
+// });
+
+app.get("/", (req, res) => {
+  connection.query("SELECT*FROM mySampleTable", function (err, rows, fields) {
+    if (!!err) {
+      console.log("hata");
+    } else {
+      console.log("Başarılı...");
+    }
+  });
+});
+
+var sunucu = http.createServer((req, res) => {
+  fs.readFile(__dirname + "/index.html", function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end("Error loading index.html");
+    }
+    res.writeHead(200);
+    res.end(data);
+  });
+});
+
+sunucu.listen(3000);
+reload(app);
