80da3f343b2708d11cf4fe2f0c0c4cef672d1e11
Murat <sapedit@hotmail.com>
2020-04-13 15:44:52 +0300

    fix: url encoded

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
+
+app.use(express.urlencoded())
+app.use(express.json());
 app.use(express.static("public"));
 
 const port = process.env.PORT || 3000;
 
+app.post("/gonder", (req, res) => {
+  console.log(req.body);
+  res.send("ok");
+});
+
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
 );
-
